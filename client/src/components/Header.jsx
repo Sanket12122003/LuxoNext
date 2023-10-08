e30e9273e8e2e39017fa8ae269f8fa04d17fee06
Sanket12122003 <sanketpawarsp1212@gmail.com>
2023-10-08 12:46:25 +0530

    update the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,9 @@ import {FaSearch} from 'react-icons/fa'
 import{Link} from 'react-router-dom'
 export default function Header() {
   return (
-    <header className='bg-slate-300 shadow-md'>
+    <header className='flex justify-between items-center max-w-8xl mx-auto p-3 bg-slate-300 shadow-md'>
 
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        
          <Link to='/'> 
        <h1  className='font-bold text-sm sm:text-xl flex flex-wrap'>
         <span className='text-green-500'> Grapes</span>
@@ -16,7 +16,7 @@ export default function Header() {
             className='bg-transparent focus:outline-none w-24 sm:w-64'/>
             <FaSearch className='text-slate-600'/>
         </form>
-        <ul className='flex gap-4'>
+        <ul className='flex gap-8'>
         <Link to='/'> 
             <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
          </Link>
@@ -29,7 +29,7 @@ export default function Header() {
          </Link>
             
         </ul>
-        </div>
+        
     </header>
   )
 }

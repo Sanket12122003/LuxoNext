5353db0ca243299508db71cd36ac04dcfdc8952a
Sanket12122003 <sanketpawarsp1212@gmail.com>
2023-10-09 22:50:53 +0530

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/errror.js';
-
+import Listing from '../models/listing.model.js'
 export const test = (req, res) => {
   res.json({
     message: 'Api route is working!',
@@ -51,3 +51,17 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUserListings= async(req, res,next) => {
+
+  if(req.user.id === req.params.id) {
+  try {
+const listings = await Listing.find({ userRef:req.params.id})
+res.status(200).json(listings)
+  } catch (error) {
+    next(error);
+  }
+  }else{
+    return next(errorHandler(401,'You can only view your listings'))
+  }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { test, updateUser ,deleteUser} from '../controllers/user.controller.js';
+import { test, updateUser ,deleteUser,getUserListings} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken,getUserListings)
 
 export default router;

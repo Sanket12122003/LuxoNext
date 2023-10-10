c68a62efd2a6dad61da9b41f3f49c2cc9c94e4c4
Sanket12122003 <sanketpawarsp1212@gmail.com>
2023-10-10 22:37:01 +0530

    listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -284,7 +284,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -301,7 +303,9 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>($ / month)</span>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                 </div>
               </div>
             )}

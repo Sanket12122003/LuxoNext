24362815b3817ce47f69712f54cfa1800b405db3
Sanket12122003 <sanketpawarsp1212@gmail.com>
2023-10-11 09:02:15 +0530

    create listing component and show listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -107,7 +107,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice}
+                    ${+listing.regularPrice - +listing.discountPrice} OFF
                 </p>
               )}
             </div>
@@ -138,7 +138,10 @@ export default function Listing() {
               </li>
             </ul>
             {currentUser && listing.userRef !== currentUser._id && !contact && (
-              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+              <button
+              onClick={() => setContact(true)}
+              className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+            >
                 Contact landlord
               </button>
             )}

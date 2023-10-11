590de481f7ff6635dda7d0952800abf2bd24a5c2
Sanket12122003 <sanketpawarsp1212@gmail.com>
2023-10-11 23:16:44 +0530

    update about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+
 
 export default function About() {
   return (
-    <div>
-      
-    </div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+    <h1 className='text-3xl text-center font-bold mb-4 text-green-800'>GrapesBazzar</h1>
+    <h1 className='text-2xl text-center font-bold mb-4 text-slate-800'>Introduction:</h1>
+    <p className='mb-4 text-slate-700'>At GrapesBazzar, we are more than just an online marketplace. we are your gateway to a world of exceptional products and seamless shopping experiences. We believe that shopping should be a joy, and selling should be a breeze, and that's what we are here to offer.</p>
+    
+    <h1 className='text-2xl text-center font-bold mb-4 text-slate-800'>Our Mission:</h1>
+    <p className='mb-4 text-slate-700'>
+
+    Our mission is simple: To connect people with the finest selection of products and services, making shopping and selling a delightful experience. We are driven by the desire to empower businesses and individuals, creating opportunities for growth and prosperity.
+    </p>
+
+    <h1 className='text-2xl text-center font-bold mb-4 text-slate-800'>Our Vision:</h1>
+    <p className='mb-4 text-slate-700'>Our vision is a marketplace where diversity and quality thrive. We aspire to build a platform that is not just a marketplace but a community, where buyers and sellers come together to discover, connect, and excel.</p>
+
+    <h1 className='text-2xl text-center font-bold mb-4 text-slate-800'>What Makes Us Unique:</h1>
+    <p className='mb-4 text-slate-700'>
+    Quality First: We're dedicated to curating products and services that meet the highest standards of quality and craftsmanship.</p>
+    <p className='mb-4 text-slate-700'>
+Seller Support: We are here to support sellers every step of the way, from setting up their shop to growing their business.
+</p>
+<p className='mb-4 text-slate-700'>
+User-Focused: Our user-friendly platform is designed to ensure that your shopping and selling experience is as smooth and enjoyable as possible.
+    </p>
+  </div>
   )
 }

293e432c6be4e42eee281d9ca6a6625a5445b8a2
Sanket12122003 <sanketpawarsp1212@gmail.com>
2023-10-11 23:24:48 +0530

    pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import useRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from './routes/listing.route.js';
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO)
 }).catch((err)=>{
     console.log(err)
 })
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -30,6 +33,12 @@ app.use("/api/user",useRouter)
 app.use("/api/auth",authRouter)
 
 app.use("/api/listing",listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next)=>{
     const statusCode= err.statusCode || 500;
